aa61961e37c26c757476bf39c8202121a3ab5089
fkdev <fkdev@nonexistent.com>
2018-12-22 11:41:04 +0100

    Hülle für Timer-Komponente hinzugefügt

---
+++ app/src/components/CountdownTimer.vue
@@ -0,0 +1,22 @@
+<template>
+  <p>Zeit bis {{ereignis}}: {{countdown}}</p>
+</template>
+
+<script>
+export default {
+  props: {
+    ereignis: "",
+    zielDatum: ""
+  },
+  computed: {
+    countdown: function() {
+      let timeLeft;
+    }
+  }
+};
+</script>
+
+<style>
+</style>
+
+
