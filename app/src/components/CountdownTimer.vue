a5ecff112505b5e14ed37c81f1e84f9d76f7ca54
fkdev <fkdev@nonexistent.com>
2018-12-24 01:00:38 +0100

    Beispiel-Code teilweise entfernt CountdownTimer Komponente berechnet Countdown

--- app/src/components/CountdownTimer.vue
+++ app/src/components/CountdownTimer.vue
@@ -4,14 +4,52 @@
 
 <script>
 export default {
+  data() {
+    return {
+      now: null
+    };
+  },
   props: {
-    ereignis: "",
-    zielDatum: ""
+    ereignis: String,
+    zielDatum: Number
   },
   computed: {
     countdown: function() {
-      let timeLeft;
+      const dayInMs = 86400000;
+      const hourInMs = 3600000;
+      const minInMs = 60000;
+      const restZeit = this.zielDatum - this.now;
+      const restTage = Math.trunc(restZeit / dayInMs);
+      const restStunden = Math.trunc(
+        (restZeit - restTage * dayInMs) / hourInMs
+      );
+      const restMinuten = Math.trunc(
+        (restZeit - restTage * dayInMs - restStunden * hourInMs) / minInMs
+      );
+      const restSekunden = Math.trunc(
+        (restZeit -
+          restTage * dayInMs -
+          restStunden * hourInMs -
+          restMinuten * minInMs) /
+          1000
+      );
+      return (
+        restTage +
+        " Tage " +
+        restStunden +
+        " Std " +
+        restMinuten +
+        " Min " +
+        restSekunden +
+        " Sek "
+      );
     }
+  },
+  beforeMount: function() {
+    let self = this;
+    setInterval(function() {
+      self.now = Date.now();
+    }, 1000);
   }
 };
 </script>
